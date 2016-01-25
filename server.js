56cadf787ea2b92224c170962400cb737ebbff88
benjaminpettus <benjaminpettus@gmail.com>
2016-01-24 19:26:55 -1000

    got edit page to render

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var path =require('path');
-
+var bodyParser = require('body-parser');
 var db = require('./models');
+var methodOverride = require('method-override');
 var Pix = db.Pix;
 var PORT = 5555;
 
@@ -10,8 +11,7 @@ var app = express();
 
 app.set('view engine', 'jade');
 app.set('views', 'views');
-
-var methodOverride = require('method-override');
+app.use(bodyParser.urlencoded({extended: false}));
 //whatever is passed in here has to be used as the query paramater in jade
 app.use(methodOverride('_method'));
 
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/gallery/new', function (req, res) {
-  res.render('new_pic', {});
+  res.render('pic_form', {});
 
 });
 
@@ -42,7 +42,31 @@ app.get('/gallery/:id', function (req, res) {
     });
 
 });
+app.get('/gallery/:id/edit', function (req, res) {
+    console.log(req.params);
+   Pix.findById(req.params.id)
+   .then(function (result){
+    var locals = {
+      id: result.id,
+      author: result.author,
+      link: result.link, 
+      description: result.description
+    };
+  res.render('edit', locals);
+});
 
+});
+
+app.post('/gallery/:id', function (req, res) {
+  Pix.create({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+    })
+    .then(function (result){
+    res.redirect('/gallery/' + result.id);
+    });
+});
 
 // sync then listen on port
 db.sequelize
